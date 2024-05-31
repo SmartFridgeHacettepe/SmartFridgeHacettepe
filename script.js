e3583a9af7ebd3bbc5df2990b0fd035b28d66b88
SmartFridgeHacettepe <smartfridgehacettepe@gmail.com>
2024-05-31 18:33:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-function toggleSidebar() {
-    const sidebar = document.getElementById('sidebar');
-    sidebar.classList.toggle('open');
+function scrollToSection(sectionId) {
+    const section = document.getElementById(sectionId);
+    if (section) {
+        const offset = 220; // Adjust this value if needed
+        const bodyRect = document.body.getBoundingClientRect().top;
+        const elementRect = section.getBoundingClientRect().top;
+        const elementPosition = elementRect - bodyRect;
+        const offsetPosition = elementPosition - offset;
+
+        window.scrollTo({
+            top: offsetPosition,
+            behavior: 'smooth'
+        });
+    }
 }
