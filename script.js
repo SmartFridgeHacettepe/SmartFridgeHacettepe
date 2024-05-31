3ec2654f4049ad09a76115d455a2e87216f76f7d
SmartFridgeHacettepe <smartfridgehacettepe@gmail.com>
2024-05-31 18:36:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function scrollToSection(sectionId) {
     const section = document.getElementById(sectionId);
     if (section) {
-        const offset = 220; // Adjust this value if needed
+        const offset = 150; // Adjusted offset to match the new padding in CSS
         const bodyRect = document.body.getBoundingClientRect().top;
         const elementRect = section.getBoundingClientRect().top;
         const elementPosition = elementRect - bodyRect;
